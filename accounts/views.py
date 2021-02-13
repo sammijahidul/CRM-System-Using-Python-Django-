24355fb21a9e8ee2c467c478178d762e25caf9a5
sammijahidul <jahidul.islam03@northsouth.edu>
2021-02-13 23:16:44 +0600

    create order button working now

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import OrderForm
@@ -35,6 +35,12 @@ def customer(request, new_value):
 def createOrder(request):
 
     form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
 
     context = {'form': form}
 
