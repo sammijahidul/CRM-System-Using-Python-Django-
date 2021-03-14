8f2273d373f7a68ea4fc418bce7fc81b88473496
sammijahidul <jahidul.islam03@northsouth.edu>
2021-03-15 00:12:55 +0600

    fixed a bug

--- first_project/settings.py
+++ first_project/settings.py
@@ -135,7 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 #SMTP Configuration
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtop.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mdjahidulsammi@gmail.com'
